f0e4465f1ee5ca65dd6a283c2057dc06482ad713
Artem Antipin <antipinart@yandex.ru>
2020-11-12 19:55:37 +0300

    firs exemple

---
+++ js1/script.js
@@ -0,0 +1,28 @@
+"use strict"
+console.log(1);
+    let number = null;
+console.log(number);
+    const person = 'yqw';
+
+    console.log(person)
+
+    const obj = {
+        name: 'fog',
+        age: 5,
+        isMared: false
+
+    };
+
+    console.log(obj.name)
+    
+    //allert ('hiar');
+    //const resullt = confirm('Auy out');
+    //console.log(resullt);
+
+    const answerr = [];
+
+    answerr[0] = prompt ("Как дела ?","")
+    answerr[1] = prompt ("Заебись ?","")
+    answerr[2] = prompt ("Охуеть ?","")
+
+    document.write(answerr)
